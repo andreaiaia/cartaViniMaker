2b6168649c92212e7f6b818f50c293a01f155339
Andrea Bianchi <bianan96@gmail.com>
2021-11-11 23:39:59 +0100

    refactor of init functions

--- src/script.js
+++ src/script.js
@@ -19,22 +19,22 @@ const addCantina = document.querySelector('#new_cantina');
 const addVino = document.querySelector('#new_vino');
 
 // Event listeners
-choiceBtns.forEach(btn => btn.addEventListener("click", (e) => {initForms(e.target.id)}));
+choiceBtns.forEach(btn => btn.addEventListener("click", (e) => {start(e.target.id)}));
 // Spicy Event Listeners
-addRegione.addEventListener("click", newRegione);
+addRegione.addEventListener("click", () => {newRegione(null)});
 
 addCantina.addEventListener("click", () => {
   const last = main.lastChild;
-  newCantina(last);
+  newCantina(last, null);
 });
 
 addVino.addEventListener("click", () => {
   const daddy = main.lastChild.lastChild;
-  newVino(daddy);
+  newVino(daddy, null);
 });
 
 // Funzioni
-function initForms(vinoScelto) {
+function initForms(vinoScelto, obj) {
   chosen = vinoScelto;
   let ita, eng;
   switch (vinoScelto) {
@@ -67,19 +67,21 @@ function initForms(vinoScelto) {
   title.innerHTML = `${ita}<span class="subtitle"> ${eng}</span>`;
   main.appendChild(title);
 
-  newRegione();
+  newRegione(obj);
 }
 
 // CREAZIONE DEGLI INPUT FIELDS
 // input nome regione
-function newRegione() {
+function newRegione(val) {
   const regione = document.createElement('DIV');
   regione.classList.add("regione");
-  regione.innerHTML = `
-      <label for="regione">Regione delle cantine:</label>
-      <input type="text" name="regione">
-      <button class="del_regione btn-del">ELIMINA REGIONE</button>
-    `;
+  const etichetta = makeLabel("regione", "Regione delle cantine:");
+  const campo = makeInput("text", "regione", val);
+  const btn = makeBtn("del-regione", "ELIMINA REGIONE");
+
+  regione.appendChild(etichetta);
+  regione.appendChild(campo);
+  regione.appendChild(btn);
 
   delItem(regione);
 
@@ -87,55 +89,75 @@ function newRegione() {
   main.appendChild(regione);
   spacing(regione, 'divider');
 
-  newCantina(regione);
+  newCantina(regione, val);
 }
 
 // input nome cantine
-function newCantina(parent) {
+function newCantina(parent, val) {
   const cantina = document.createElement('DIV');
   cantina.classList.add("cantina");
-  cantina.innerHTML = `
-    <p class="input_cantina">
-      <label for="cantina">Cantina:</label>
-      <input type="text" name="cantina">
-      <button class="del_cantina btn-del">ELIMINA CANTINA</button>
-    </p>
-  `;
+  const p = document.createElement('P');
+  p.classList.add("input_cantina");
+  const etichetta = makeLabel("cantina", "Cantina:");
+  const campo = makeInput("text","cantina", val);
+  const btn = makeBtn("del_cantina", "ELIMINA CANTINA");
+
+  p.appendChild(etichetta);
+  p.appendChild(campo);
+  p.appendChild(btn);
+  cantina.appendChild(p);
 
   delItem(cantina);
 
   parent.appendChild(cantina);
 
-  newVino(cantina);
+  newVino(cantina, val);
 }
 
-// input vino
-function newVino(parent) {
+// input vino - questa funzione è orribile e si può fare decisamente meglio
+function newVino(parent, val) {
   const vino = document.createElement('DIV');
   vino.classList.add('vino');
-  vino.innerHTML = `
-    <p class="inputs">
-      <label for="anno_tipo">Anno e tipo:</label>
-      <input type="text" name="anno_tipo">
-    </p>
-    <p class="inputs">
-      <label for="nome">Nome:</label>
-      <input type="text" name="nome">
-    </p>
-    <p class="inputs">
-      <label for="denom">Indicazione:</label>
-      <input type="text" name="denom">
-    </p>
-    <p class="inputs">
-      <label for="prezzo">Costo: (in 00,00):</label>
-      <input type="text" name="costo">
-    </p>
-    <button class="del_vino btn-del">ELIMINA VINO</button>
-  `;
+  const p1 = document.createElement('P');
+  p1.classList.add("inputs");
+  const etichetta1 = makeLabel("anno_tipo", "Anno e tipo:");
+  const campo1 = makeInput("text", "anno_tipo", val);
+  p1.appendChild(etichetta1);
+  p1.appendChild(campo1);
+
+  const p2 = document.createElement('P');
+  p2.classList.add("inputs");
+  const etichetta2 = makeLabel("nome", "Nome:");
+  const campo2 = makeInput("text", "nome", val);
+  p2.appendChild(etichetta2);
+  p2.appendChild(campo2);
+
+  const p3 = document.createElement('P');
+  p3.classList.add("inputs");
+  const etichetta3 = makeLabel("denom", "Indicazione:");
+  const campo3 = makeInput("text", "denom", val);
+  p3.appendChild(etichetta3);
+  p3.appendChild(campo3);
+
+  const p4 = document.createElement('P');
+  p4.classList.add("inputs");
+  const etichetta4 = makeLabel("prezzo", "Costo (in 00,00):");
+  const campo4 = makeInput("text", "costo", val);
+  p4.appendChild(etichetta4);
+  p4.appendChild(campo4);
+
+  const btn = makeBtn("del_vino", "ELIMINA VINO");
+
+  vino.appendChild(p1);
+  vino.appendChild(p2);
+  vino.appendChild(p3);
+  vino.appendChild(p4);
+  vino.appendChild(btn);
   
   delItem(vino);
 
   parent.appendChild(vino);
+
 }
 
 // DELETING THINGS
@@ -168,7 +190,7 @@ function saveRegioni() {
   selRegioni.forEach(regione => {
     const input = regione.querySelector('INPUT');
     const info = {};
-    info[`${input.name}`] = `${input.value}`;
+    info[`${input.name}`] = `${input.val}`;
     info['cantine'] = saveCantine(regione);
     regioni[`regione${i}`] = info;
     i++;
@@ -185,7 +207,7 @@ function saveCantine(regione) {
   selCantine.forEach(cantina => {
     const input = cantina.querySelector('INPUT');
     const info = {};
-    info[`${input.name}`] = `${input.value}`;
+    info[`${input.name}`] = `${input.val}`;
     info['vini'] = saveVini(cantina);
     cantine[`cantina${i}`] = info;
     i++;
@@ -202,7 +224,7 @@ function saveVini(cantina) {
     const inputs = vino.querySelectorAll('INPUT');
     const info = {};
     inputs.forEach(inp => {
-      info[`${inp.name}`]=`${inp.value}`;
+      info[`${inp.name}`]=`${inp.val}`;
     });
     vini[`vino${i}`] = info;
     i++;
@@ -210,5 +232,37 @@ function saveVini(cantina) {
   return vini;
 }
 
+// Helpers to create DOM elements
+function makeLabel(forAtt, description) {
+  const etichetta = document.createElement('LABEL');
+  etichetta.setAttribute("for",forAtt);
+  etichetta.innerHTML = description;
+  return etichetta;
+}
 
-initForms("rosso");
+function makeInput(type, name, val) {
+  const campo = document.createElement('INPUT');
+  campo.setAttribute("type",type);
+  campo.setAttribute("name",name);
+  campo.value = val ? val : "";
+  return campo;
+}
+
+function makeBtn(theClass, innerContent) {
+  const btn = document.createElement('BUTTON');
+  btn.classList.add(theClass);
+  btn.classList.add("btn-del");
+  btn.innerHTML = innerContent;
+  return btn;
+}
+
+// Controlla se ci sono salvataggi precedenti, se sì li carica
+function start(menuScelto) {
+  if (localStorage.getItem(menuScelto)) {
+    initForms(menuScelto, JSON.parse(localStorage.getItem(menuScelto)));
+  } else {
+    initForms(menuScelto, null);
+  }
+}
+
+start("rosso");
